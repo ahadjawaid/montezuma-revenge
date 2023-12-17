07d2fbe4561e68279a5c8daf76d07073bcd32a2a
ahadjawaid <94938815+ahadjawaid@users.noreply.github.com>
2023-12-17 07:39:12 -0600

    feat: added check to make sure checkpoint or model is there

--- visualize.py
+++ visualize.py
@@ -3,19 +3,20 @@ from PIL import Image
 from trainer import DQNTrainer
 import torch
 import gym
+from gym.wrappers import TimeLimit
 
 
 def visualize_episode(
         env_name: str, 
         checkpoint_path: str = None, 
         model=None, 
-        model_params: dict = None, 
         device: str = None
     ):
+    assert checkpoint_path is not None or model is not None, "Either checkpoint_path or model must be provided"
     device = get_device(device)
 
     env = gym.make(env_name, render_mode="human")
-    if model is None:
+    if model is None and checkpoint_path:
         trainer = DQNTrainer.load(checkpoint_path)
 
     state = torch.from_numpy(env.reset()[0]).float().unsqueeze(0).to(device)
