632dd6d8e86a86d1ff8a6d30ada3296f22272509
ahadjawaid <94938815+ahadjawaid@users.noreply.github.com>
2023-12-17 08:57:48 -0600

    fix: change while loop to include not truncated

--- visualize.py
+++ visualize.py
@@ -20,14 +20,15 @@ def visualize_episode(
         trainer = DQNTrainer.load(checkpoint_path)
 
     state = torch.from_numpy(env.reset()[0]).float().unsqueeze(0).to(device)
-    done = False
-    while not done:
+    done = truncated = False
+    while not done and not truncated:
         env.render()
         q_values = trainer.online_model(state)
         action = q_values.argmax(dim=1)
-        state, _, done, _, _ = env.step(action)
+        # action = trainer.exploration.get_action(q_values)
+        state, _, done, truncated, _ = env.step(action)
         state = torch.from_numpy(state).float().unsqueeze(0).to(device)
-
+    print(f"Done: {done}, Truncated: {truncated}")
     env.close()
 
 
