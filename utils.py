e7b170b4fc6fc1c1ac97a2324869bd34f2afac53
ahadjawaid <94938815+ahadjawaid@users.noreply.github.com>
2023-12-17 07:08:35 -0600

    feat: create get_sorted_checkpoints function

--- utils.py
+++ utils.py
@@ -1,8 +1,14 @@
 import torch
-
+from pathlib import Path
 
 def get_device(device: str = None):
     return torch.device(
         device if device else "cuda" if torch.cuda.is_available() else "mps"
         if torch.backends.mps.is_available() else "cpu"
     )
+
+def get_sorted_checkpoints(checkpoint_dir: str):
+    checkpoint_dir_path = Path(checkpoint_dir)
+    checkpoints = list(checkpoint_dir_path.glob("*.pt"))
+    checkpoints.sort(key=lambda x: x.stat().st_mtime)
+    return checkpoints
